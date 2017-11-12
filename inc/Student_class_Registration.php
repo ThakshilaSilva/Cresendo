3249d4b5209d8ad54594fab0119de83fb35b3de0
“WinmaH” <winma.15@cse.mrt.ac.lkcd>
2017-11-12 20:34:50 +0530

    student_registration

--- inc/Student_class_Registration.php
+++ inc/Student_class_Registration.php
@@ -20,44 +20,78 @@ function operation($name1,$class){
 
         $year = $split_class[4];
         $term = $split_class[6];
-        $Class_id = $split_class[8];
+        $Class_id = $split_class[13];
+        $type=$split_class[11];
+        echo $type;
 
 
         #check whether student has payed for the class.
-        $fee_id = "";
-        $stmt2 = $con->prepare("SELECT Fee_id FROM Fee WHERE Student_id=? AND Class_id = ? AND Year=? AND Term=? AND Instrument_id=?");
-        $stmt2->bind_param("sssss", $id, $Class_id, $year, $term, $Instrument_id);
+        $paid_date = "";
+        $stmt2 = $con->prepare("SELECT PaidDate FROM FeeAdmission WHERE S_ID=? ");
+        $stmt2->bind_param("s", $id);
         $stmt2->execute();
-        $stmt2->bind_result($fee_id);
+        $stmt2->bind_result($paid_date);
         $stmt2->fetch();
         $stmt2->close();
         #Check whether student has already registered.
 
         $count = 0;
-        $stmt3 = $con->prepare("SELECT Count(Student_id) FROM participate WHERE Student_id=? AND Instrument_id=? AND Class_id=? AND Year =? AND Term=?");
-        $stmt3->bind_param("sssss", $id, $Instrument_id, $Class_id, $year, $term);
+        $stmt3 = $con->prepare("SELECT Count(S_ID) FROM participate WHERE S_ID=? AND Class_id=?");
+        $stmt3->bind_param("ss", $id,$Class_id);
         $stmt3->execute();
         $stmt3->bind_result($count);
         $stmt3->fetch();
         $stmt3->close();
 
+        #chek whether there is enough space
 
-        if ($fee_id == "") {
-            echo "<script>alert('Please Complete the Payment for the class!')</script>";
+
+        $number = 0;
+        $stmt4 = $con->prepare("SELECT Count(S_ID) FROM participate WHERE  Class_id=?");
+        $stmt4->bind_param("s",$Class_id);
+        $stmt4->execute();
+        $stmt4->bind_result($number);
+        $stmt4->fetch();
+        $stmt4->close();
+        echo $number;
+
+        $capacity=0;
+        $stmt4 = $con->prepare("SELECT Capacity FROM class NATURAL JOIN Class_room WHERE Class_id=?");
+        $stmt4->bind_param("s",$Class_id);
+        $stmt4->execute();
+        $stmt4->bind_result($capacity);
+        $stmt4->fetch();
+        $stmt4->close();
+        echo $capacity;
+
+
+        if($type=="Individual" and $number>=1){
+            echo "<script>alert('Cannot be Registered as the class is full')</script>";
             echo "<script>window.open('main_admin_window.php','_self')</script>";
-
+        }elseif($type=="Group" and $number>=$capacity){
+            echo "<script>alert('Cannot be Registered as the class is full')</script>";
+            echo "<script>window.open('main_admin_window.php','_self')</script>";
+        } elseif ($paid_date == "") {
+            echo "<script>alert('Student Cannot be registered to the Class. Register fee Not Paid!')</script>";
+            echo "<script>window.open('main_admin_window.php','_self')</script>";
         } elseif ($count == 1) {
             echo "<script>alert('Student has already Registered for this class!')</script>";
-            echo "<script>window.open('main_admin_window.php','_self')</script>";
-
+            #echo "<script>window.open('main_admin_window.php','_self')</script>";
         } else {
-            $stmt1 = $con->prepare("INSERT INTO participate(Student_id,Instrument_id,Class_id,Year,Term) VALUES (?, ?, ?, ?, ?)");
-            $stmt1->bind_param("sssss", $id, $Instrument_id, $Class_id, $year, $term);
-            $stmt1->execute();
-            $stmt1->close();
+
+            $stmt5 = $con->prepare("INSERT INTO participate(S_ID,Class_id) VALUES (?, ?)");
+            $stmt5->bind_param("ss", $id, $Class_id);
+            $result5=$stmt5->execute();
+            $stmt5->close();
             $con->close();
+            if($result5){
+
             echo "<script>alert('Registered to the class Successfully!')</script>";
             echo "<script>window.open('main_admin_window.php','_self')</script>";
+            } else{
+                echo "<script>alert('Error Occur in Registering!!')</script>";
+                echo "<script>window.open('main_admin_window.php','_self')</script>";
+            }
         }
     }catch (mysqli_sql_exception $e){
         echo "<script>alert('Error Occur in connecting to the Database!')</script>";
