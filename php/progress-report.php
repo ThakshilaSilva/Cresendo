14fa6d52c78c4a5c2565994c8fc484460bd1d540
Madushi <madushi.15@cse.mrt.ac.lk>
2017-12-17 15:33:55 +0530

    chart

--- php/progress-report.php
+++ php/progress-report.php
@@ -34,21 +34,67 @@ $stmt3->close();
 $_SESSION['dic']=$dic;
 $_SESSION['grade']=$Grade;
 $_SESSION['exam']=$Exam;
+$_SESSION['fname']=$FirstName;
+$_SESSION['lname']=$LastName;
 }
 ?>
 <?php
 $dic=$_SESSION['dic'];
 $Grade=$_SESSION['grade'];
 $Exam=$_SESSION['exam'];
+$FirstName=$_SESSION['fname'];
+$LastName=$_SESSION['lname'];
+$Instrument=$_SESSION['instrument'];
 ?>
 <!DOCTYPE html>
 <html>
+<body>
 
+
+<header id="header">
+    <!-- <p ALIGN="RIGHT"> Logged in as: <?php /*echo $NAME;*/?> <a href="login.php" id="logout">(logout)</a></p>-->
+    <h1 style="text-align: center" ><strong>CRESCENDO MUSIC ACADEMY </strong></h1>
+    <!--  <span class="avatar"><img src="images/avatar.jpg" alt="" /></span> -->
+</header>
+<form class="view class" method="get" action="#">
+
+    <div class="container">
+        <h1 style="text-align: center"><strong>Progress Report </strong></h1>
+        <p style="font-size: 140%" align="center"><strong><?php echo $FirstName.' '.$LastName;?> </strong></p>
+        <p style="font-size: 140%" align="center"><strong><?php echo $Instrument.' '. 'Class';?> </strong></p>
+        <?php $ar[]=array();?>
+        <?php foreach($dic as $key => $value):?>
+
+            <div class="progress">
+                <div class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow=<?php echo $value;?> aria-valuemin="100" aria-valuemax="100" style="width:<?php echo $value;?>%">
+                    <?php echo 'Marks for'.' '.$key." = ".$value; $ar[]=$value?>
+
+
+                </div>
+            </div>
+
+
+
+        <?php endforeach;?>
+        <?php $size=sizeof($ar)-1;
+        $total=array_sum($ar);
+        $avg=$total/$size;?>
+        <p style="font-size: 140%" align="left"><strong><?php echo 'Average marks='.number_format($avg,2,'.','');?> </strong></p>
+        <p style="font-size: 140%" align="center"><strong><?php echo '';?> </strong></p>
+
+    </div>
+
+
+
+
+</form>
+
+</body>
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="GView Progress" content="width=device-width, initial-scale=1">
+    <meta name="View Progress" content="width=device-width, initial-scale=1">
 
     <title>Class Details</title>
 
@@ -63,45 +109,18 @@ $Exam=$_SESSION['exam'];
 
 
 
-<div class="container">
-
-    <?php foreach($dic as $key => $value):?>
-
-        <div class="progress">
-            <div class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow=<?php echo $value;?> aria-valuemin="0" aria-valuemax="100" style="width:<?php echo ($value/$max_count)*100;?>%">
-                <?php echo $key."    ".$value?>
-            </div>
-        </div>
 
 
-
-    <?php endforeach;?>
-
-</div>
-
-
-<body>
-
-
-<header id="header">
-    <!-- <p ALIGN="RIGHT"> Logged in as: <?php /*echo $NAME;*/?> <a href="login.php" id="logout">(logout)</a></p>-->
-    <h1 style="text-align: center"><strong>CRESCENDO MUSIC ACADEMY </strong></h1>
-    <!--  <span class="avatar"><img src="images/avatar.jpg" alt="" /></span> -->
-</header>
-
-
-</body>
 <head>
     <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/demo.css">
-    <link rel="stylesheet" href="../css/main.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/bootstra_charts.css">
-    <link rel="stylesheet" href="../css/main.css">
+
 
 
 </head>
